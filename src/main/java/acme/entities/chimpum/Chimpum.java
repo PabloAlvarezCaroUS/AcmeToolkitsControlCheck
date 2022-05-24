c82ee9a1a743062eac9101490b85c94d3a352144
PabloAlvarezCaroUS <100071510+PabloAlvarezCaroUS@users.noreply.github.com>
2022-05-24 18:08:33 +0200

    Task 001: Chimpum created
    Commit previous to the 22.6 update

---
+++ src/main/java/acme/entities/chimpum/Chimpum.java
@@ -0,0 +1,82 @@
+/*
+ * Chirp.java
+ *
+ * Copyright (C) 2012-2022 Rafael Corchuelo.
+ *
+ * In keeping with the traditional purpose of furthering education and research, it is
+ * the policy of the copyright owner to permit non-commercial use and redistribution of
+ * this software. It has been tested carefully, but it is not guaranteed for any particular
+ * purposes. The copyright owner does not offer any warranties or representations, nor do
+ * they accept any liabilities with respect to them.
+ */
+
+package acme.entities.chimpum;
+
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.ManyToOne;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.validation.Valid;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Past;
+import javax.validation.constraints.Pattern;
+
+import org.hibernate.validator.constraints.Length;
+import org.hibernate.validator.constraints.URL;
+
+import acme.entities.artifacts.Artifact;
+import acme.framework.datatypes.Money;
+import acme.framework.entities.AbstractEntity;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class Chimpum extends AbstractEntity {
+
+	// Serialisation identifier -----------------------------------------------
+
+	protected static final long		serialVersionUID	= 1L;
+
+	// Attributes -------------------------------------------------------------
+	
+	@NotBlank
+	@Length(max = 100)
+	protected String 			title;
+
+	@Column(unique = true)
+	@Pattern(regexp = "^[A-Z]{3}-[0-9]{3}(-[A-Z])?$")
+	protected String			pattern;
+	
+	@Temporal(TemporalType.TIMESTAMP)
+	@Past
+	@NotNull
+	protected Date				creationMoment;
+	
+	//no sé como es exactamente
+	protected Date				period;
+	
+	@Valid
+	@NotNull
+	protected Money				budget;
+	
+	@URL
+	protected String			link;
+	
+
+	// Derived attributes -----------------------------------------------------
+	
+	public String getCode() {
+		String year;
+		return this.pattern + "-yy/mm/dd";
+	}
+
+	// Relationships ----------------------------------------------------------
+	
+
+}
